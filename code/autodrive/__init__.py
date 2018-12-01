a30387d47c053662a8b3a263f7089a202fbcb284
Ankur Roy Chowdhury <ankurrc@tamu.edu>
2018-11-30 20:45:21 -0600

    Heavily modified carla_rl: carla_environment_wrapper.py and human_play.py

---
+++ code/autodrive/__init__.py
@@ -0,0 +1,5 @@
+'''
+Created Date: Friday November 30th 2018
+Last Modified: Friday November 30th 2018 7:15:06 pm
+Author: ankurrc
+'''
---
+++ code/autodrive/carla_rl/__init__.py
@@ -0,0 +1,5 @@
+'''
+Created Date: Friday November 30th 2018
+Last Modified: Friday November 30th 2018 7:18:13 pm
+Author: ankurrc
+'''
